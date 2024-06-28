51f090cd7c93e47b7b8062b3164292616bacd559
Mary Spain <mkspain8@gmail.com>
2024-06-28 18:22:48 -0500

    Initial Commit

---
+++ Fingersign!/Fingersign_App.swift
@@ -0,0 +1,17 @@
+//
+//  Fingersign_App.swift
+//  Fingersign!
+//
+//  Created by Mary Spain on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Fingersign_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
